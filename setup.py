b8cc0a4602720246a0fc914f3147de85d8396ecc
Nawinkumar97 <149937435+Nawinkumar97@users.noreply.github.com>
2023-11-13 00:09:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="math_quiz",
+    version="0.1.0",
+    description="solving_maths_problem_to_score_points",
+    author="N",
+    author_email="nawinkumar355@gmail.com",
+    url="https://github.com/Nawinkumar97/DSSS_homework_2",
+    packages=["math_quiz"],
+    install_requires=[],
+)
