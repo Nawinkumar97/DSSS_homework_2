5dd6efa1aacd2259fe1470d6c653c55be9b0c9fa
Nawinkumar97 <149937435+Nawinkumar97@users.noreply.github.com>
2023-11-13 01:02:09 +0100

    Add__init__.py

--- math_quiz/__init__.py
+++ math_quiz/__init__.py
@@ -3,3 +3,15 @@
 from .math_quiz import math_quiz  # Assuming your module is named 'math_quiz'
 
 __all__ = ["math_quiz"]
+
+
+def function_B():
+    return None
+
+
+def function_A():
+    return None
+
+
+def function_C():
+    return None
